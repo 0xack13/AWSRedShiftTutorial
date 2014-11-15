c5cb2e121603751bfc5b49e5c99e08d672ea06db
Saleh Albatati <0xack13@gmail.com>
2014-11-15 13:07:28 +0300

    Walkthrough Script - Init

---
+++ sqlScriptWalkthrough.sql
@@ -0,0 +1,14 @@
+SELECT * FROM users ORDER BY userid LIMIT 100;
+
+select * from users;
+
+-- Get definition for the sales table.
+SELECT *    
+FROM pg_table_def    
+WHERE tablename = 'sales';    
+
+-- Find total sales on a given calendar date.
+SELECT sum(qtysold) 
+FROM   sales, date 
+WHERE  sales.dateid = date.dateid 
+AND    caldate = '2008-01-05';
